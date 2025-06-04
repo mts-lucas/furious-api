2dad41ea1119175636fb2ee6d5098b0d53dd05fd
mts-lucas <lmateus1067@outlook.com>
2025-06-04 12:28:40 -0300

    chore: config main script

--- furious_api/main.py
+++ furious_api/main.py
@@ -1,9 +1,23 @@
 import argparse
+import re
+from furious_api.starters import start_file_type_project
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Script com um argumento posicional obrigatório.")
-    parser.add_argument("project_name", type=str, help="Nome do projeto, primeiro argumento posicional obrigatório.")
+def main():
+    parser = argparse.ArgumentParser(description="CLI para iniciar projeto FastAPI.")
+    parser.add_argument("command", type=str, help="Comando, por ex: startapp")
+    parser.add_argument("project_name", type=str, help="Nome do projeto")
     args = parser.parse_args()
 
-    from starters import start_file_type_project
-    start_file_type_project(base_dir=args.project_name)
+    pattern = r"^[a-z]+(_[a-z]+)*$"
+    if not re.match(pattern, args.project_name):
+        print("Nome do projeto inválido! Use apenas letras minúsculas e underscores (_) para separar palavras.")
+        exit(1)
+
+    if args.command == "startapp":
+        start_file_type_project(base_dir=args.project_name)
+    else:
+        print(f"Comando desconhecido: {args.command}")
+        exit(1)
+
+if __name__ == "__main__":
+    main()
