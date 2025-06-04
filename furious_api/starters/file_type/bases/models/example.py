0d3ffa42bc546cd70efac143a5d3878da77e1bf3
mts-lucas <lmateus1067@outlook.com>
2025-06-04 12:07:06 -0300

    feat: add models

---
+++ furious_api/starters/file_type/bases/models/example.py
@@ -0,0 +1,11 @@
+from app.core.database import Base
+from sqlalchemy import Column, Float, Integer, String
+
+
+class ItemExample(Base):
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    description = Column(String, nullable=True)
+    price = Column(Float)
+    tax = Column(Float, nullable=True)
